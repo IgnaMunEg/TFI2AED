3c41aa8f06e8a4d687d026f89f5689f2136392c9
Ignacio <imunozegea@gmail>
2020-12-16 14:44:48 -0300

    Ignacio
    Agregada la opcion de pedir el veterinario mercedor del bono

--- ADMINISTRACION.cpp
+++ ADMINISTRACION.cpp
@@ -7,6 +7,7 @@ FILE*Turnos;
 FILE*mascotas;
 FILE*veterinario;
 FILE*usuario;
+FILE*bono;
 
 struct FechaNacimiento{
 	int dd,mm,aa;	
@@ -31,13 +32,18 @@ struct Turnoss{
 
 struct Veterinario{
 	char Contras[35],NombreApe[60],Telefono[25]; 
-	int Matricula,Dni;	
+	int Matricula,Dni,Atenciones;	
 }vet;
 
 struct Usuario{
 	char usuario[10],contr[10],nombreYape[60];
 }user;
 
+struct Bono{
+	int maxAten,MatriculaBono,DniBono;
+	char NombreBono[60],TelefonoBono[25];
+	
+}bon;
 
 int z=0,j=0,i=0,opp=0,op1=0,op2=0,op3=0,band=0,validar=0,encontrado=0,val=0;
 char op,oppp;
@@ -46,8 +52,9 @@ char op,oppp;
 
 
 bool ValidarVeterinario(char cad[]);
-void RegistrarVeterinarios(FILE*veterinario);
+void RegistrarVeterinarios(FILE*veterinario,FILE*bono);
 void RegistrarUsuario(FILE*usuario);
+void VeterinarioBono(FILE*bono);
 
 
 
@@ -58,29 +65,26 @@ main(){
 	printf("   Menu administracion"); 
  	printf("\n 1 - Registrar Veterinario ");               
 	printf("\n 2 - Registrar Usuario Asistente ");              
-	printf("\n 3 - Atenciones por Veterinarios ");
-	printf("\n 4 - Ranking de Veterinarios por Atenciones");
- 	printf("\n 5 - Cerrar la aplicacion");
+	printf("\n 3 - Veterinario merecedor del bono ");
+ 	printf("\n 4 - Cerrar la aplicacion");
  	printf("\n\n Ingrese una opcion: ");scanf("%d",&op1);
  	
  			switch(op1){
 						case 1:{
-							RegistrarVeterinarios(veterinario);
+							RegistrarVeterinarios(veterinario,bono);
 						break;}	
 						case 2:{
 							RegistrarUsuario(usuario);
 						break;}	
 						case 3:{
-							
+							VeterinarioBono(bono);
 						break;}	
 						case 4:{
-							
-						break;}	
-						case 5:{
 							printf("\n Finalizado");
 							getch();
-						break;}	
+						break;}		
 					}
+	return 0;
 }
 
 bool ValidarVeterinario(char cad[]){
@@ -96,10 +100,11 @@ bool ValidarVeterinario(char cad[]){
 	}
 }
 
-void RegistrarVeterinarios(FILE*veterinario){
+void RegistrarVeterinarios(FILE*veterinario,FILE*bono){
 	char cadena[60];
 	int listo=0;
 	veterinario=fopen("Veterinarios.dat","a+b");
+	bono=fopen("Bono.dat","a+b");
 	do{
 		system("cls");
 		_flushall();
@@ -110,6 +115,14 @@ void RegistrarVeterinarios(FILE*veterinario){
 		printf("\n Digite dni: ");scanf("%d",&vet.Dni);
 		_flushall();
 		printf("\n Digite la matricula: ");scanf("%d",&vet.Matricula);
+		printf("\n Digite las atenciones del veterinario: ");scanf("%d",&vet.Atenciones);
+			if(vet.Atenciones>bon.maxAten){
+				bon.maxAten=vet.Atenciones;
+				strcpy(bon.NombreBono,vet.NombreApe);
+				strcpy(bon.TelefonoBono,vet.Telefono);
+				bon.MatriculaBono=vet.Matricula;
+				bon.DniBono=vet.Dni;		
+			}
 		do{
 			_flushall();
 			printf("\n Digite la contrase%ca: ",164);gets(cadena);
@@ -134,6 +147,7 @@ void RegistrarVeterinarios(FILE*veterinario){
 		}
 	}while(listo==0);
 	fclose(veterinario);
+
 }
 
 void RegistrarUsuario(FILE*usuario){
@@ -157,4 +171,17 @@ void RegistrarUsuario(FILE*usuario){
 		}
 	}while(lista==0);
 	fclose(usuario);
+
 }
+
+void VeterinarioBono(FILE*bono){
+	
+	bono=fopen("Bono.dat","a+b");
+	printf("\n El veterinario merecedor del bono con un total de %d atenciones es: ",bon.maxAten);
+	printf("\n -");puts(bon.NombreBono);
+	printf("\n DNI: %d",bon.DniBono);
+	printf("\n Matricula: %d",bon.MatriculaBono);
+	printf("\n Telefono: ");gets(bon.TelefonoBono);	
+	
+}
+
